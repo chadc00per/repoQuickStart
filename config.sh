2e075bc7b930d16ce28e9003785864e46de03569
PLAYSET <chad@playset.co>
2024-09-07 14:34:14 -0400

    config

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 # Configuration file
 
-# The folder where repositories are stored, relative to the home directory
+# The name of this repository
+REPO_NAME="repoQuickStart"
+
+# The folder where all repositories are stored, relative to the home directory
 # Cannot be your home directory.
 REPO_DIR="test"
 
@@ -19,4 +22,8 @@ SUCCESS_NOTIFICATIONS=false
 # Record logging of this app?
 LOGGING_ENABLED=true
 
-export REPO_DIR LOG_FILE INTERVAL ERROR_NOTIFICATIONS SUCCESS_NOTIFICATIONS LOGGING_ENABLED
+# Select Mac of Linux
+SYSTEM_TYPE="mac"
+#SYSTEM_TYPE="linux"
+
+export REPO_DIR LOG_FILE INTERVAL ERROR_NOTIFICATIONS SUCCESS_NOTIFICATIONS LOGGING_ENABLED SYSTEM_TYPE
